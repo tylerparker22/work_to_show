2560e164c2469f5e319a9b40baf8f6fe788e583a
tylerparker22 <tyler.parker2216@gmail.com>
2026-05-06 15:28:02 -0500

    Remove local folders, keep on GitHub

--- Classes Spring 2026/Econometrics/Stock market analysis 2-9.r
+++
@@ -1,9 +0,0 @@
-# Stock market analysis 2.9
-# %% install packages
-install.packages("quantmod")
-library(quantmod)
-
-install.packages("PerformanceAnalytics")
-library(PerformanceAnalytics)
-
-# %%
